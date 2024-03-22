4c7ed7d41fac639557bf177a4447d4f7bd51e5fa
DAVID RONDIO <davidrondio@MacBook-Pro-de-DAVID.local>
2024-03-22 15:10:03 +0100

    add assets & change app layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Events</h1>
+      <Button>Button</Button>
+    </main>
+  );
+}
